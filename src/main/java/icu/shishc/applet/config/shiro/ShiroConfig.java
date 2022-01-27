0623c2d564c99da68be0ee7959d8619a993c8d15
shishc9 <52554717+shishc9@users.noreply.github.com>
2022-01-27 20:42:27 +0800

    feat: [shiro] add shiro config

---
+++ src/main/java/icu/shishc/applet/config/shiro/ShiroConfig.java
@@ -0,0 +1,37 @@
+package icu.shishc.applet.config.shiro;
+
+import icu.shishc.applet.filter.TokenFilter;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        Map<String, Filter> filterMap = new LinkedHashMap<>();
+        filterMap.put("token", new TokenFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
+
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        filterChainDefinitionMap.put("", "");
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(TokenRealm tokenRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(tokenRealm);
+        return securityManager;
+    }
+
+}
