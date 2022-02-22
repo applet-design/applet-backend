050b8673a8b863389a14989fd1f78da20583bd42
shishc9 <52554717+shishc9@users.noreply.github.com>
2022-02-22 16:31:04 +0800

    feat

--- src/main/java/icu/shishc/applet/service/NoticeService.java
+++ src/main/java/icu/shishc/applet/service/NoticeService.java
@@ -7,10 +7,13 @@ import java.util.List;
 
 public interface NoticeService {
 
+    // 新增一条通知
     Long addNotice(NoticeParam noticeParam);
 
+    // 通过通知id查看这条通知
     Notice getNoticeById(Long noticeId);
 
+    // 获取通知列表. (按照时间进行倒序，最新的放在最前面)
     List<Notice> getNoticeList();
 
 }
