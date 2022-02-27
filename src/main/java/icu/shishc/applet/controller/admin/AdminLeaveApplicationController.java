1c28c09e9393aa2054a04838338741b749fb7c85
shishc9 <52554717+shishc9@users.noreply.github.com>
2022-02-27 16:06:02 +0800

    add admin leave module

---
+++ src/main/java/icu/shishc/applet/controller/admin/AdminLeaveApplicationController.java
@@ -0,0 +1,15 @@
+package icu.shishc.applet.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/admin")
+public class AdminLeaveApplicationController {
+
+    // 查看所有申请
+
+    // 查看所有待审批
+
+    // 审批某个申请
+}
